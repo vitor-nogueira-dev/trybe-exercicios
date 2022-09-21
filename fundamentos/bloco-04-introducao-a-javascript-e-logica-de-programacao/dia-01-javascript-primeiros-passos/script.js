59884f38327746bcad337023caa1b4bc84a04ab9
Vitor Nogueira <vslnogueira96@gmail.com>
2022-09-21 11:37:58 -0300

    performing part v

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -80,3 +80,23 @@ if (note >= 80){
 }
 
 // Você está na nossa lista de espera
+
+// Parte V
+
+let currentHour = 16; 
+let message = ''; 
+
+if (currentHour >= 22){
+    message = 'Não deveríamos comer nada, é hora de dormir'
+} else if (currentHour >= 18 && currentHour < 22){
+    message = 'Rango da noite, vamos jantar :D'
+} else if (currentHour >= 14 && currentHour < 18){
+    message - "Vamos fazer um bolo pro café da tarde?"
+} else if (currentHour >= 11 && currentHour < 14) {
+    message = "Hora do almoço!!!"
+} else if (currentHour >= 4 && currentHour < 11) {
+    message = 'Hmmm, cheiro de café recém passado.'
+} else {
+    message = "Hora inválida, digite a hora correta"
+}
+console.log(message);
