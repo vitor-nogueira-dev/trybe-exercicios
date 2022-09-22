b292727070291ff2f193dafc0cf42e9fc336077b
Vitor Nogueira <vslnogueira96@gmail.com>
2022-09-22 11:26:22 -0300

    fixation exercises

---
+++ fundamentos/secao-04-introducao-a-javascript-e-logica-de-progamacao/dia-02-javascript-array-e-loop-for/fixação/fixacao-README.md
@@ -0,0 +1,6 @@
+# Exercícios de Fixação 🚀
+1 - Obtenha o valor “Serviços” do array menu
+
+2 - Procure o índice do valor “Portfólio” do array menu
+
+3 - Adicione o valor “Contato” no final do array menu
---
+++ fundamentos/secao-04-introducao-a-javascript-e-logica-de-progamacao/dia-02-javascript-array-e-loop-for/fixação/fixacao.js
@@ -0,0 +1,19 @@
+// Parte I
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf('Serviços');
+
+console.log(menuServices);
+
+// 1 
+// Parte II
+let indexOfPortfolio = menu.indexOf('Portfólio')
+
+console.log(indexOfPortfolio);
+
+// 2
+// Parte III
+menu.push('Contato');
+
+console.log(menu);
+
+// [ 'Home', 'Serviços', 'Portfólio', 'Links', 'Contato' ]
