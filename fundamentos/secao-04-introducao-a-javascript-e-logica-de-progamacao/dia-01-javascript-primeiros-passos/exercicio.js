a775f0e2e46d78bb6a365aa18fb5435a674e4b4a
Vitor Nogueira <vslnogueira96@gmail.com>
2022-09-21 21:15:04 -0300

    updating exercises

--- fundamentos/secao-04-introducao-a-javascript-e-logica-de-progamacao/dia-01-javascript-primeiros-passos/exercicio.js
+++ fundamentos/secao-04-introducao-a-javascript-e-logica-de-progamacao/dia-01-javascript-primeiros-passos/exercicio.js
@@ -97,22 +97,90 @@ switch (transform) {
 }
 // Rei: pode se mover em todas as direções
 
-let nota = 89; 
+let nota = 89;
 
-if (nota >= 90 && nota <= 100){ 
-    console.log('Sua nota convertida  em conceito é A')
-} else if (nota >= 80 && nota <= 89){
-    console.log('Sua nota convertida em conceito é B')
-} else if (nota >= 70 && nota <= 79){
-    console.log('Sua nota convertida em conceito é C')
-} else if (nota >= 60 && nota <= 69){
-    console.log('Sua nota convertida em conceito é D')
-} else if (nota >= 50 && nota <= 59){
-    console.log('Sua nota convertida em conceito é E')
-} else if (nota < 50 && nota >= 0){
-    console.log('Sua nota convertida em conceito é F')
+if (nota >= 90 && nota <= 100) {
+  console.log("Sua nota convertida  em conceito é A");
+} else if (nota >= 80 && nota <= 89) {
+  console.log("Sua nota convertida em conceito é B");
+} else if (nota >= 70 && nota <= 79) {
+  console.log("Sua nota convertida em conceito é C");
+} else if (nota >= 60 && nota <= 69) {
+  console.log("Sua nota convertida em conceito é D");
+} else if (nota >= 50 && nota <= 59) {
+  console.log("Sua nota convertida em conceito é E");
+} else if (nota < 50 && nota >= 0) {
+  console.log("Sua nota convertida em conceito é F");
 } else {
-    console.log('Erro, valor inválido!');
+  console.log("Erro, valor inválido!");
 }
 
 // Sua nota convertida em conceito é B
+
+let num1 = 2;
+let num2 = 35;
+let num3 = 65;
+
+if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+// True
+
+let teste1 = 1;
+let teste2 = 5;
+let teste3 = 10;
+
+if (teste1 % 2 !== 0 || teste2 % 2 !== 0 || teste3 % 2 !== 0) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+//True
+
+let custoProduto = 2;
+let valorVenda = 5;
+let impostoSobreProduto = 0.2;
+
+if (custoProduto >= 0 && valorVenda >= 0) {
+  let custoTotalProduto = custoProduto * impostoSobreProduto + custoProduto;
+  let valorLucro = (valorVenda - custoTotalProduto) * 1000;
+  console.log("Seu lucro total nessa venda é de: " + valorLucro);
+} else {
+  console.log("Valor incorreto!");
+}
+
+// Seu lucro total nessa venda é de: 2600
+
+let salarioBruto = 5000;
+let impostoInss;
+let impostoIr;
+
+//tabela INSS
+if (salarioBruto <= 1556.94) {
+  impostoInss = salarioBruto * 0.08;
+} else if (salarioBruto <= 2594.92) {
+  impostoInss = salarioBruto * 0.09;
+} else if (salarioBruto <= 5189.82) {
+  impostoInss = salarioBruto * 0.11;
+} else if (salarioBruto >= 5189.83) {
+  impostoInss = 570.88;
+}
+let salarioBase = salarioBruto - impostoInss;
+// tabela IR
+if (salarioBruto <= 1903.98) {
+  impostoIr = 0;
+} else if (salarioBruto <= 2826.65) {
+  impostoIr = (salarioBase * 0.075) - 142.80;
+} else if (salarioBruto <= 3751.05) {
+  impostoIr = (salarioBase * 0.15) - 354.80;
+} else if (salarioBruto <= 4664.68) {
+  impostoIr = (salarioBase * 0.225) - 636.13;
+} else {
+  impostoIr = (salarioBase * 0.275) - 869.36;
+}
+
+console.log('Seu salário líquido a receber é: ' + (salarioBase - impostoIr));
+ 
+// Seu salário líquido a receber é: 4095.61
