41ba9c8a216d2b8a7f1e98cd5e79b3c861d638f2
Vitor Nogueira <vslnogueira96@gmail.com>
2022-09-26 15:56:12 -0300

    Exercícios de Fixação

--- fundamentos/secao-04-introducao-a-javascript-e-logica-de-progamacao/dia-04-javascript-objetos-e-funcoes/fixação/README.md
+++ fundamentos/secao-04-introducao-a-javascript-e-logica-de-progamacao/dia-04-javascript-objetos-e-funcoes/fixação/README.md
@@ -2,3 +2,5 @@
 
 - Crie uma variável player e atribua um objeto contendo as variáveis listadas abaixo.
 
+- Usando o objeto abaixo, utilize for/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+
--- fundamentos/secao-04-introducao-a-javascript-e-logica-de-progamacao/dia-04-javascript-objetos-e-funcoes/fixação/fixacao.js
+++ fundamentos/secao-04-introducao-a-javascript-e-logica-de-progamacao/dia-04-javascript-objetos-e-funcoes/fixação/fixacao.js
@@ -1,3 +1,5 @@
+// FOR/IN
+
 let player = {
   name: "Marta",
   lastName: "Silva",
@@ -7,5 +9,55 @@ let player = {
     silver: 3,
   },
 };
-console.log(player)
+console.log(player);
 
+let cars = ["Mercedes", "Camaro", "BMW"];
+
+for (let index in cars) {
+  console.log(index, cars[index]);
+}
+
+let carros = {
+  type: "Fiat",
+  model: "500",
+  color: "white",
+};
+
+for (let index in carros) {
+  console.log(index, car[index]);
+}
+
+// FOR/of
+
+let food = ["hamburguer", "bife", "acarajé"];
+for (let value of food) {
+  console.log(value);
+}
+//resultado: hamburguer, bife, acarajé;
+
+let names = {
+  person1: "João",
+  person2: "Maria",
+  person3: "Jorge",
+};
+
+for (let index in names) {
+  console.log("Olá, " + names[index] + ".");
+}
+// Olá, João.
+// Olá, Maria.
+// Olá, Jorge.
+
+let car = {
+  model: "A3 Sedan",
+  manufacturer: "Audi",
+  year: 2020,
+};
+
+for (let index in car){
+    console.log(index, car [index]);
+}
+
+// model A3 Sedan
+// manufacturer Audi
+// year 2020
