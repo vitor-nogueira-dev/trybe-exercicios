1c3915e63e56377997cda41ff7df2bf50e0d1b11
Vitor Nogueira <vslnogueira96@gmail.com>
2022-09-28 19:18:08 -0300

    Resolução dos exercícios de seletores

---
+++ fundamentos/secao-5-dom-eventos-e-web-storage/exercicios-seletores/script.js
@@ -0,0 +1,14 @@
+document.getElementById('header-container').style.backgroundColor = 'mediumseagreen';
+document.querySelector('.emergency-tasks').style.backgroundColor = 'coral';
+document.querySelector('.no-emergency-tasks').style.backgroundColor = 'gold';
+
+
+// document.querySelector('section.emergency-tasks').style.backgroundColor = '';
+document.querySelector('#footer-container').style.backgroundColor = 'darkslategray'
+
+// document.getElementsByClassName('.title-urgent').style.backgroundColor = 'black'
+
+document.querySelectorAll('.title-urgent')[0].style.backgroundColor = 'purple'
+document.querySelectorAll('.title-urgent')[1].style.backgroundColor = 'purple'
+document.querySelectorAll('.title-no-urgent')[0].style.backgroundColor = 'black'
+document.querySelectorAll('.title-no-urgent')[1].style.backgroundColor = 'black'
