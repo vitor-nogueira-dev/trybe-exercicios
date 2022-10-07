6714d4e7e51283c23d31c7748a7afb419dc47ace
Vitor Nogueira <vslnogueira96@gmail.com>
2022-10-07 15:43:43 -0300

    Organizando diretórios

---
+++ fundamentos/secao-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascipt-e-frameworks-css - cópia/exercicios/script.js
@@ -0,0 +1,16 @@
+const submit = document.querySelector('#submit');
+
+function eventoClick(event){ 
+  event.preventDefault();
+}
+submit.addEventListener('click', eventoClick);
+const simENao = document.querySelector('#sim-e-nao').value;
+function quemAutorizar(){
+  if ( simENao === on){
+    const botao = document.createElement('input'); 
+    console.log(botao);
+
+  }
+}
+
+
