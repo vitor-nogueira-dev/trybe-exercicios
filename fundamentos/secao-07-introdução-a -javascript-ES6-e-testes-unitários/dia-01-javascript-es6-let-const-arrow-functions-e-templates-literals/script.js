5d1afc37882268f520d15b60fa6deb8a02cc8376
Vitor Nogueira <vslnogueira96@gmail.com>
2022-10-17 11:31:44 -0300

    Exercícios do dia [4]

--- fundamentos/secao-07-introdução-a -javascript-ES6-e-testes-unitários/dia-01-javascript-es6-let-const-arrow-functions-e-templates-literals/script.js
+++ fundamentos/secao-07-introdução-a -javascript-ES6-e-testes-unitários/dia-01-javascript-es6-let-const-arrow-functions-e-templates-literals/script.js
@@ -9,5 +9,11 @@ const testingScope = (escopo) => {
   }
   // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
 }
-
 testingScope(true);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = oddsAndEvens.sort((a, b) => a - b); // Quando precisar colocar um array de números em ordem crescente, é necessário passar dois parâmetros, para que ele compare o primeiro com o segundo e verifique qual é menor para ordenar.
+
+console.log(`Os números ${sortOddsAndEvens} se encontram ordenados de forma crescente!`);
+//Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!
