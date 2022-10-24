060d403480f90817be4c00b296004916ebc1b6b9
Vitor Nogueira <vslnogueira96@gmail.com>
2022-10-24 11:41:25 -0300

    Praticando exercício [2]

--- fundamentos/secao-08-higher-order-functions-javascript-es6/dia-01-javascript-es6-introducao-hofs/exercicios.js
+++ fundamentos/secao-08-higher-order-functions-javascript-es6/dia-01-javascript-es6-introducao-hofs/exercicios.js
@@ -12,5 +12,25 @@ const newEmployees = (callback) => {
   }
   return employees
 }
-
 console.log(newEmployees(func));
+
+// exercício 2
+
+const check = (numeroApostado) => {
+  const numeroGerado = () => Math.floor(Math.random() * 5);
+  if (numeroApostado === numeroGerado) {
+    return 'Parabéns você ganhou!' 
+    } else {
+    return 'Não foi dessa vez, tente novamente!'
+    }
+}
+// console.log(numbers(2));
+
+// Estrutura com HOFs
+
+// const numberCheck = (numeroApostado, numeroGerado) => numeroApostado === numeroGerado;
+// const lotery = (numeroApostado, callback) => {
+//   const numeroGerado = () => Math.floor((Math.random() * 5) + 1);
+//   return callback(numeroApostado, numeroApostado) ? 'Parabéns você ganhou!' : 'Não foi dessa vez, tente novamente!';
+// }
+// console.log(lotery(3, numberCheck));
