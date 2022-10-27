a6fd4f220cf866a998017c84c2cf49017db34ff1
Vitor Nogueira <vslnogueira96@gmail.com>
2022-10-27 14:51:18 -0300

    Resolução do exercício [3]

---
+++ fundamentos/secao-08-higher-order-functions-javascript-es6/dia-04-hofs-reduce/exercicios/exercise3.js
@@ -0,0 +1,30 @@
+const names = [
+  "Aanemarie",
+  "Adervandes",
+  "Akifusa",
+  "Abegildo",
+  "Adicellia",
+  "Aladonata",
+  "Abeladerco",
+  "Adieidy",
+  "Alarucha",
+];
+
+const expectedResult = 20;
+
+// Dado o array de nomes, utilize o reduce para retornar a quantidade de vezes em que aparece a letra a, maiúscula ou minúscula.
+
+const letterA = () => {
+  let totalLettersA = 0;
+  names.forEach((name) => {
+    const letrasDosNomes = name.split("");
+    totalLettersA += letrasDosNomes.reduce((acc, curr) => {
+      if (curr.toLowerCase() === "a"){
+        return acc + 1;
+      } 
+      return acc
+    }, 0);
+  })
+ return totalLettersA;
+};
+console.log(letterA());
