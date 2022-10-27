a6f5fa5a7dfbf7afbd54f6cc98bbb20fb6121773
Vitor Nogueira <vslnogueira96@gmail.com>
2022-10-27 15:57:41 -0300

    Resolução do exercício [5]

---
+++ fundamentos/secao-08-higher-order-functions-javascript-es6/dia-04-hofs-reduce/exercicios/exercise4.js
@@ -0,0 +1,23 @@
+// Criar um novo array [] de objetos [{name: nome aluno, average: media das notas}]
+// const expected = [
+//   { name: 'Pedro Henrique', average: 7.8 },
+//   { name: 'Miguel', average: 9.2 },
+//   { name: 'Maria Clara', average: 8.8 },
+// ];
+
+const students = ["Pedro Henrique", "Miguel", "Maria Clara"];
+const grades = [
+  [9, 8, 10, 7, 5],
+  [10, 9, 9, 10, 8],
+  [10, 7, 10, 8, 9],
+];
+
+const studentAverage = () => {
+  return students.map((element, index) => ({
+    name: element,
+    average:
+      grades[index].reduce((acc, curr) => acc + curr, 0) / grades[index].length,
+  }));
+};
+
+console.log(studentAverage());
