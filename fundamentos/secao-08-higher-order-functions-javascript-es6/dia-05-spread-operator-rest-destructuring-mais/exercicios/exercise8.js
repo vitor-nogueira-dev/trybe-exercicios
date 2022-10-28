28f6c4f1a34f290224a93e6c642bce474e229fbd
Vitor Nogueira <vslnogueira96@gmail.com>
2022-10-28 19:20:54 -0300

    Resolução exercício [8]

--- fundamentos/secao-08-higher-order-functions-javascript-es6/dia-05-spread-operator-rest-destructuring-mais/exercicios/exercise8.js
+++ fundamentos/secao-08-higher-order-functions-javascript-es6/dia-05-spread-operator-rest-destructuring-mais/exercicios/exercise8.js
@@ -1,12 +1,14 @@
-
-
 // Escreva uma função greet que, dado o nome de uma pessoa, retorna uma mensagem de cumprimento. Um parâmetro adicional pode ser passado para alterar o cumprimento utilizado:
 
 // De olho na dica 👀: use o default params.
 
 // escreva greet abaixo
-
+const greet = (person, saudacao = "Hi") => {
+  typeof saudacao === undefined ? "Hi" : person;
+  return `${saudacao} ${person}`;
+  // `${person} ${saudacao}`
+};
 // Retornos esperados:
-console.log(greet('John')); // 'Hi John'
-console.log(greet('John', 'Good morning')); // 'Good morning John'
-console.log(greet('Isabela', 'Oi')); // 'Oi Isabela'
+console.log(greet("John")); // 'Hi John'
+console.log(greet("John", "Good morning")); // 'Good morning John'
+console.log(greet("Isabela", "Oi")); // 'Oi Isabela'
