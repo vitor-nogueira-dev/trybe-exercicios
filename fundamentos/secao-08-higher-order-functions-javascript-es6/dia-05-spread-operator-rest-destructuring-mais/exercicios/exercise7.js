57b301a7ad921281d8bfff61ea17cd8b0a3f717a
Vitor Nogueira <vslnogueira96@gmail.com>
2022-10-28 19:04:54 -0300

    Resolução exercício [7]

--- fundamentos/secao-08-higher-order-functions-javascript-es6/dia-05-spread-operator-rest-destructuring-mais/exercicios/exercise7.js
+++ fundamentos/secao-08-higher-order-functions-javascript-es6/dia-05-spread-operator-rest-destructuring-mais/exercicios/exercise7.js
@@ -23,7 +23,12 @@ const ships = [
 
 // escreva shipLength abaixo
 
-// retorno esperado
+// const [a, b, c] = ships
+const shipLength = ({name, length, measurementUnit}) => {
+    return `${name} is ${length} ${measurementUnit} long`
+};
+
+// // retorno esperado
 console.log(shipLength(ships[0])); // 'Titanic is 269.1 meters long'
 console.log(shipLength(ships[1])); // 'Queen Mary 2 is 1132 feet long'
 console.log(shipLength(ships[2])); // 'Yamato is 256 meters long'
