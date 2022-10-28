d7f6bcb4d91804994269c79112415d1290d4c1f7
Vitor Nogueira <vslnogueira96@gmail.com>
2022-10-28 18:38:41 -0300

    Criação dos arquivos 7, 8, e 9

---
+++ fundamentos/secao-08-higher-order-functions-javascript-es6/dia-05-spread-operator-rest-destructuring-mais/exercicios/exercise8.js
@@ -0,0 +1,12 @@
+
+
+// Escreva uma função greet que, dado o nome de uma pessoa, retorna uma mensagem de cumprimento. Um parâmetro adicional pode ser passado para alterar o cumprimento utilizado:
+
+// De olho na dica 👀: use o default params.
+
+// escreva greet abaixo
+
+// Retornos esperados:
+console.log(greet('John')); // 'Hi John'
+console.log(greet('John', 'Good morning')); // 'Good morning John'
+console.log(greet('Isabela', 'Oi')); // 'Oi Isabela'
---
+++ fundamentos/secao-08-higher-order-functions-javascript-es6/dia-05-spread-operator-rest-destructuring-mais/exercicios/exercise9.js
@@ -0,0 +1,16 @@
+
+
+// Existe um objeto yearSeasons que representa as estações do ano. Cada estação é uma chave deste objeto, e o valor de cada chave é um array de strings, representando os meses daquela estação. A partir deste objeto, desestruture as estações do ano e espalhe-as em um array de meses do ano.
+
+// De olho na dica 👀: use object destructuring e spread operator.
+
+// OBS: As estações do ano aqui descritas têm base nas estações do hemisfério norte.
+
+
+const yearSeasons = {
+  spring: ['March', 'April', 'May'],
+  summer: ['June', 'July', 'August'],
+  autumn: ['September', 'October', 'November'],
+  winter: ['December', 'January', 'February'],
+};
+// yearSeasons: estações do ano.
