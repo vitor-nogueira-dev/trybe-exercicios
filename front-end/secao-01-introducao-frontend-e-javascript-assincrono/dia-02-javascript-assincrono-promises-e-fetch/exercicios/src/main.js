9f4c799a1721c82d9d81f771f568097f1d912f00
Vitor Nogueira <vslnogueira96@gmail.com>
2022-11-10 12:10:11 -0300

    Criação dos arquivos e diretório dos exercícios

---
+++ front-end/secao-01-introducao-frontend-e-javascript-assincrono/dia-02-javascript-assincrono-promises-e-fetch/exercicios/src/main.js
@@ -0,0 +1,5 @@
+console.log('teste');
+
+// Já criei os arquivos necessários, inciei o npm e o vite. 
+// Já linkei o js com html e já está rodando o vite.
+// já realizei commit da criacao dos arquivos.
