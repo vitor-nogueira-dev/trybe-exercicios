e4292d68bdc0a0b811ef172dadeb240b8b229518
Vitor Nogueira <vslnogueira96@gmail.com>
2022-11-10 11:24:56 -0300

    Resolução dos exercícios de fixação

---
+++ front-end/secao-01-introducao-frontend-e-javascript-assincrono/dia-02-javascript-assincrono-promises-e-fetch/script.js
@@ -0,0 +1,7 @@
+// {
+//   quote: "Alguma frase inspiradora",
+//   author: "Pessoa autora da frase"
+// }
+
+fetch("https://api.goprogram.ai/inspiration")
+.then(response => console.log(response));
