8b68209981e61f72fb2aab3c254e67b8c8aca7d3
Vitor Nogueira <vslnogueira96@gmail.com>
2022-11-10 12:02:33 -0300

    Resolução dos exercícios bônus

--- front-end/secao-01-introducao-frontend-e-javascript-assincrono/dia-02-javascript-assincrono-promises-e-fetch/script.js
+++
@@ -1,7 +0,0 @@
-// {
-//   quote: "Alguma frase inspiradora",
-//   author: "Pessoa autora da frase"
-// }
-
-fetch("https://api.goprogram.ai/inspiration")
-.then(response => console.log(response));
