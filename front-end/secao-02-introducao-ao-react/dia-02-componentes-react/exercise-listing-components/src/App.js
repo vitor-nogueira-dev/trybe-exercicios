303d5b5d8cd9dcf8c79542df206df5db19b924bd
Vitor Nogueira <vslnogueira96@gmail.com>
2022-11-23 14:58:47 -0300

    Ajustando exercício list componentes

--- front-end/secao-02-introducao-ao-react/dia-02-componentes-react/exercise-listing-components/src/App.js
+++ front-end/secao-02-introducao-ao-react/dia-02-componentes-react/exercise-listing-components/src/App.js
@@ -1,38 +1,32 @@
-import UserProfile from "./UserProfile";
-import React from "react";
-import "./App.css";
+import UserProfile from './UserProfile';
+import React from 'react';
+import './App.css';
+
+
+const pessoas = [
+  {
+    id: 102,
+    name: 'João',
+    email: 'joao@gmail.com',
+    avatar: 'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png',
+  },
+  {
+    id: 77,
+    name: 'Amélia',
+    email: 'amelia@gmail.com',
+    avatar: 'https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png',
+  }];
 
 class App extends React.Component {
   render() {
-    const pessoas = [
-      {
-        id: 102,
-        name: "João",
-        email: "joao@gmail.com",
-        avatar:
-          "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png",
-      },
-      {
-        id: 77,
-        name: "Amélia",
-        email: "amelia@gmail.com",
-        avatar:
-          "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png",
-      },
-    ];
-    return (
-      <div className="App">
-        {pessoas.map((pessoa, id) => {
-          return (
-            <ul>
-              <li key={id} className="App">
-                <UserProfile user={pessoa} />
-              </li>
-            </ul>
-          );
-        })}
-      </div>
-    );
+      const teste = pessoas.map((pessoa) => {
+       return  <UserProfile  key={pessoa.id} user={ pessoa }/> });
+       return (
+        <ul className='App'>
+          <li>{teste}</li>
+        </ul>
+       );
+      }
   }
-}
+
 export default App;
