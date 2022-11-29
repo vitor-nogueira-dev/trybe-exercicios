db89c3368ad700387f0f19bf2f6d1e91186009a0
Vitor Nogueira <vslnogueira96@gmail.com>
2022-11-29 10:53:21 -0300

    Declarando o estado inicial com Public Class Fields

--- front-end/secao-03-componentes-com-estado-eventos-e-formularios-react/fancy-buttons/src/App.js
+++ front-end/secao-03-componentes-com-estado-eventos-e-formularios-react/fancy-buttons/src/App.js
@@ -5,11 +5,17 @@ import React, { Component } from "react";
 export default class App extends Component {
   constructor() {
     super();
-    this.state = {
-      numeroDeCliques: 0,
-    };
+    // Essa é uma forma de declarar o estado inicial, é necessário que ela esteja dentro do escopo do constructor
+    // this.state = { 
+    //   numeroDeCliques: 0,
+    // };
     this.terca = this.terca.bind(this);
   }
+  // Essa é outra forma de declarar o estado inicial, mais simples e menos verbosa. Ela é utilizada fora do escopo do constructor();
+  state = {
+    numeroDeCliques: 0,
+  };
+
   terca = () => {
     this.setState((estadoAnterior, _props) => ({
       numeroDeCliques: estadoAnterior.numeroDeCliques + 1,
