0050d5418a53fde04a14a7d73a4aafe895d8d2fd
Vitor Nogueira <vslnogueira96@gmail.com>
2022-11-30 11:01:17 -0300

    Exercício de Fixação fieldset - e Componente Controlado

--- front-end/secao-03-componentes-com-estado-eventos-e-formularios-react/dia-02-formularios-react/my-form-1.0/src/components/Forms.jsx
+++ front-end/secao-03-componentes-com-estado-eventos-e-formularios-react/dia-02-formularios-react/my-form-1.0/src/components/Forms.jsx
@@ -2,34 +2,61 @@ import React, { Component } from "react";
 
 export default class Forms extends Component {
   state = {
-    stackPreferida: '',
-    nome: '',
-    email: '',
-    textArea: '',
+    stackPreferida: "",
+    nome: "",
+    email: "",
+    textArea: "",
+    recomendacao: false,
   };
 
-  handleTextArea = (event) => {
-    this.setState({stackPreferida: event.target.value})
-  }
+  handleChange = ({ target }) => {
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const { name } = target;
+    this.setState({ [name]: value });
+  };
   render() {
     return (
       <form method="GET">
-        <h1>Qual sua stack preferida ?</h1>
-        <select name="stackPreferida">
-          <option value="">Frontend</option>
-          <option value="">Backend</option>
-          <option value="">FullStack</option>
-        </select>
-        <p></p>
-        <label htmlFor="nome">Nome:</label>
-        <input type="text" id="nome" />
-        <p></p>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" />
-        <p></p>
-        <textarea placeholder="Como está sendo sua experiência na Trybe?" rows={10} cols={30} onChange={this.handleTextArea}></textarea>
-        <p>{this.state.stackPreferida}</p>
-        <button type="submit">Enviar</button>
+        <fieldset>
+          <legend>Trybe</legend>
+          <h1>Qual sua stack preferida ?</h1>
+          <select name="stackPreferida" onChange={this.handleChange}>
+            <option>Frontend</option>
+            <option>Backend</option>
+            <option>FullStack</option>
+          </select>
+          <p></p>
+          <label>Nome:</label>
+          <input type="text" name="nome" onChange={this.handleChange} />
+          <p></p>
+          <label>Email:</label>
+          <input type="email" name="email" onChange={this.handleChange} />
+          <p></p>
+          <textarea
+            placeholder="Como está sendo sua experiência na Trybe?"
+            rows={10}
+            cols={30}
+            onChange={this.handleChange}
+            name="textArea"
+          ></textarea>
+          <p>{this.state.stackPreferida}</p>
+          <button type="submit">Enviar</button>
+          <p></p>
+          <label>Recomendaria a Trybe a um amigo?</label>
+          <input
+            type="checkbox"
+            name="recomendacao"
+            onChange={this.handleChange}
+          />{" "}
+          Sim
+          <input
+            type="checkbox"
+            name="recomendacao"
+            onChange={this.handleChange}
+          />{" "}
+          Não
+          <input type="file" />
+        </fieldset>
       </form>
     );
   }
