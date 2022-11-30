ba6a5965c9ba2a8e7b1d85781fb75aacc598a184
Vitor Nogueira <vslnogueira96@gmail.com>
2022-11-30 09:37:02 -0300

    Finalizando exercícios fixação React State

--- front-end/secao-03-componentes-com-estado-eventos-e-formularios-react/fancy-buttons/src/App.js
+++ front-end/secao-03-componentes-com-estado-eventos-e-formularios-react/fancy-buttons/src/App.js
@@ -5,30 +5,79 @@ import React, { Component } from "react";
 export default class App extends Component {
   constructor() {
     super();
+    this.one = this.one.bind(this);
+    this.two = this.two.bind(this);
+    this.three = this.three.bind(this);
+
     // Essa é uma forma de declarar o estado inicial, é necessário que ela esteja dentro do escopo do constructor
-    // this.state = { 
-    //   numeroDeCliques: 0,
+    // this.state = {
+    //   one: 0,
+    //   two: 0,
+    //   three: 0,
     // };
-    this.terca = this.terca.bind(this);
   }
   // Essa é outra forma de declarar o estado inicial, mais simples e menos verbosa. Ela é utilizada fora do escopo do constructor();
   state = {
-    numeroDeCliques: 0,
+    one: 0,
+    two: 0,
+    three: 0,
   };
 
-  terca = () => {
-    this.setState((estadoAnterior, _props) => ({
-      numeroDeCliques: estadoAnterior.numeroDeCliques + 1,
-    }));
-  };
-
+  quantidadeDeCliques(num) {
+    if (num === 0) {
+      return "white";
+    }
+    return num % 2 === 0 ? "green" : "white";
+  }
+  one() {
+    const { one } = this.state;
+    this.setState(
+      ({ one }) => ({
+        one: one + 1,
+      }),
+      () => {
+        console.log(`Botão 1 ${this.quantidadeDeCliques(one)}`);
+      }
+    );
+  }
+  two() {
+    const { two } = this.state;
+    this.setState(
+      ({ two }) => ({
+        two: two + 1,
+      }),
+      () => {
+        console.log(`Botão 2 ${this.quantidadeDeCliques(two)}`);
+      }
+    );
+  }
+  three() {
+    const { three } = this.state;
+    this.setState(
+      ({ three }) => ({
+        three: three + 1,
+      }),
+      () => {
+        console.log(`Botão 3 ${this.quantidadeDeCliques(three)}`);
+      }
+    );
+  }
   render() {
-    const { numeroDeCliques } = this.state;
+    const { one, two, three } = this.state;
     return (
       <div>
-        <button type="button" onClick={this.terca}>
-          {numeroDeCliques}
-        </button>
+        <button
+          style={{ backgroundColor: this.quantidadeDeCliques(one) }}
+          onClick={this.one}
+        >{`Cliques no botão 1: ${one}`}</button>
+        <button
+          style={{ backgroundColor: this.quantidadeDeCliques(two) }}
+          onClick={this.two}
+        >{`Cliques no botão 2: ${two}`}</button>
+        <button
+          style={{ backgroundColor: this.quantidadeDeCliques(three) }}
+          onClick={this.three}
+        >{`Cliques no botão 3: ${three}`}</button>
       </div>
     );
   }
