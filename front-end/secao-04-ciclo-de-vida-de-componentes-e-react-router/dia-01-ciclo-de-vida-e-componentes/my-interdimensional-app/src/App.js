7233b11c8c1776fb73fa635be6817ae7d433b5d0
Vitor Nogueira <vslnogueira96@gmail.com>
2022-12-08 11:43:15 -0300

    Finalizando exercícios de fixação

--- front-end/secao-04-ciclo-de-vida-de-componentes-e-react-router/dia-01-ciclo-de-vida-e-componentes/my-interdimensional-app/src/App.js
+++ front-end/secao-04-ciclo-de-vida-de-componentes-e-react-router/dia-01-ciclo-de-vida-e-componentes/my-interdimensional-app/src/App.js
@@ -1,11 +1,43 @@
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import DadJoke from './components/DadJoke'
 
-function App() {
-  return (
-    <div className="App">
-      Olá
-    </div>
-  );
+export default class App extends Component {
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     characters: [],
+  //   }
+  // };
+
+  // componentDidMount = () => { // aqui poderia ter uma função diferente fetchCharacters() por exemplo, e aí teríamos que chamá-la dentro da componentDidMount, ou podemos fazer o fetch direto nessa funçào atual.
+  //   fetch('https://rickandmortyapi.com/api/character')
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     this.setState({characters: data.results})
+  //   })
+  // };
+
+  render() {
+    // const { characters } = this.state;
+    // console.log(characters)
+    return (
+      <div className="App">
+        <DadJoke />
+        {/* <h1>
+          Rick and Morty Characters
+        </h1>
+        <div className="body">
+          {characters.map(({ name, image }) => {
+              return (
+                <div className="container" key={name}>
+                  <h3>{name}</h3>
+                  <img src={image} alt={name}/>
+                </div>
+              )
+            })}
+        </div> */}
+      </div>
+    )
+  }
 }
-
-export default App;
